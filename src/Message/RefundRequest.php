08ffb8a677a5f5dd6121b9851c49aa072738005b
Sujip Thapa <sudiptpa@gmail.com>
2019-07-24 06:21:40 +0000

    Apply fixes from StyleCI

--- src/Message/RefundRequest.php
+++ src/Message/RefundRequest.php
@@ -30,7 +30,7 @@ class RefundRequest extends AbstractRequest
     public function getRequestHeaders()
     {
         return [
-            'Content-Type' => 'application/xml',
+            'Content-Type'  => 'application/xml',
             'Cache-Control' => 'no-cache',
         ];
     }
@@ -89,6 +89,6 @@ class RefundRequest extends AbstractRequest
             $endPoint = $this->testEndpoint;
         }
 
-        return $endPoint . $this->methodName;
+        return $endPoint.$this->methodName;
     }
 }
