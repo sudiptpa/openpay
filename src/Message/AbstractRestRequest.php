e081a2481ac71268b67221a32e303b4b048e2dc4
Sujip Thapa <sudiptpa@users.noreply.github.com>
2020-10-10 15:52:23 +0545

    Added RestPingRequest, AbstractRestRequest (#7)
    * Added RestGateway
    
    * Added void() request
    
    * Added RestPingRequest, AbstractRestRequest
    
    * Apply fixes from StyleCI

---
+++ src/Message/AbstractRestRequest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Omnipay\Openpay\Message;
+
+/**
+ * Class AbstractRestRequest.
+ */
+abstract class AbstractRestRequest extends \Omnipay\Common\Message\AbstractRequest
+{
+    /**
+     * @var string
+     */
+    protected $liveEndpoint = 'https://api.myopenpay.com.au/v1/merchant/';
+
+    /**
+     * @var string
+     */
+    protected $testEndpoint = 'https://api.training.myopenpay.com.au/v1/merchant/';
+
+    /**
+     * @return string
+     */
+    public function getApiKey()
+    {
+        return $this->getParameter('apiKey');
+    }
+
+    /**
+     * @param $value
+     *
+     * @return string
+     */
+    public function setApiKey($value)
+    {
+        return $this->setParameter('apiKey', $value);
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiToken()
+    {
+        return $this->getParameter('apiToken');
+    }
+
+    /**
+     * @param $value
+     *
+     * @return string
+     */
+    public function setApiToken($value)
+    {
+        return $this->setParameter('apiToken', $value);
+    }
+
+    /**
+     * @return array
+     */
+    public function getHeaders()
+    {
+        return [
+            'Content-Type'  => 'application/json',
+            'Accept'        => 'application/json',
+            'Authorization' => 'Basic '.base64_encode("{$this->getApiKey()}:{$this->getApiToken()}"),
+            'Cache-Control' => 'no-cache',
+            'Connection'    => 'close',
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    protected function getEndpoint()
+    {
+        return $this->getTestMode() ? $this->testEndpoint : $this->liveEndpoint;
+    }
+
+    /**
+     * @param $data
+     * @param array $headers
+     * @param $status
+     *
+     * @return \Omnipay\Openpay\Message\AbstractResponse
+     */
+    protected function createResponse($data, $headers = [], $status = 404)
+    {
+        return $this->response = new AbstractResponse($this, $data, $headers);
+    }
+}
