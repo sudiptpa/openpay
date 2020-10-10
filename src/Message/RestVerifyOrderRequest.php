a2c4d3511f8f70964bd0a4a8c4e61dbe941e119c
Sujip Thapa <sudiptpa@gmail.com>
2020-10-10 18:24:18 +0545

    Added Verify, Void Request

---
+++ src/Message/RestVerifyOrderRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Omnipay\Openpay\Message;
+
+/**
+ * Class RestVerifyOrderRequest.
+ */
+class RestVerifyOrderRequest extends AbstractRestRequest
+{
+    public function getData()
+    {
+        $this->validate('orderId');
+
+        return [
+            'orderId' => $this->getOrderId(),
+        ];
+    }
+
+    public function sendData($data = [])
+    {
+        $headers = $this->getHeaders();
+
+        $url = $this->getEndpoint();
+
+        $response = $this->httpClient->get($url, $headers, $data)->send();
+
+        $data = json_decode($response->getBody(), true);
+
+        return $this->createResponse($data, $response->getHeaders(), $response->getStatusCode());
+    }
+
+    /**
+     * @return string
+     */
+    protected function getEndpoint()
+    {
+        return parent::getEndpoint().vsprintf('orders/%s', [$this->getOrderId()]);
+    }
+
+    /**
+     * @param $data
+     * @param array $headers
+     * @param $status
+     *
+     * @return \Omnipay\Openpay\Message\RestVerifyOrderResponse
+     */
+    protected function createResponse($data, $headers = [], $status = 404)
+    {
+        return $this->response = new RestVerifyOrderResponse($this, $data, $headers, $status);
+    }
+}
