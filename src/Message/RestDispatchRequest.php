725fbb54209288bd28024cc36d70efff9d206b5b
Sujip Thapa <sudiptpa@users.noreply.github.com>
2020-10-10 18:17:36 +0545

    Added DispatchRequest (#10)

---
+++ src/Message/RestDispatchRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Omnipay\Openpay\Message;
+
+/**
+ * Class RestDispatchRequest.
+ */
+class RestDispatchRequest extends AbstractRestRequest
+{
+    public function getData()
+    {
+        $this->validate('orderId');
+
+        return [
+            'orderId' => $this->getOrderId(),
+        ];
+    }
+
+    public function sendData($data = [])
+    {
+        $headers = $this->getHeaders();
+
+        $url = $this->getEndpoint();
+
+        $response = $this->httpClient->post($url, $headers, json_encode($data))->send();
+
+        $data = json_decode($response->getBody(), true);
+
+        return $this->createResponse($data, $response->getHeaders(), $response->getStatusCode());
+    }
+
+    /**
+     * @return string
+     */
+    protected function getEndpoint()
+    {
+        return parent::getEndpoint().vsprintf('orders/%s/dispatch', [$this->getOrderId()]);
+    }
+
+    /**
+     * @param $data
+     * @param array $headers
+     * @param $status
+     *
+     * @return \Omnipay\Openpay\Message\RestDispatchResponse
+     */
+    protected function createResponse($data, $headers = [], $status = 404)
+    {
+        return $this->response = new RestDispatchResponse($this, $data, $headers, $status);
+    }
+}
