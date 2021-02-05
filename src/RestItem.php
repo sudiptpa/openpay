d95500413b7a69f921c367b354df0574a6aa24bc
Chris Seufert <chris@modd.com.au>
2021-02-05 14:36:37 +1100

    added new item for rest api

---
+++ src/RestItem.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Omnipay\Openpay;
+
+use Omnipay\Common\Helper;
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+/**
+ * Cart Item.
+ *
+ * This class defines a single cart item in the Omnipay system.
+ *
+ * @see ItemInterface
+ */
+class RestItem extends \Omnipay\Common\Item implements RestItemInterface
+{
+
+    public function getItemGroup()
+    {
+        return $this->parameters->get('itemGroup');
+    }
+
+    public function setItemGroup($v)
+    {
+        $this->parameters->set('itemGroup',$v);
+    }
+
+    public function getItemCode()
+    {
+        return $this->parameters->get('itemCode');
+    }
+
+    public function setItemCode($v)
+    {
+        $this->parameters->set('itemCode',$v);
+    }
+
+    public function getItemGroupCode()
+    {
+        return $this->parameters->get('itemGroupCod');
+    }
+
+    public function setItemGroupCode($v)
+    {
+        $this->parameters->set('itemGroupCod',$v);
+    }
+
+    public function getTotalPrice()
+    {
+        return $this->parameters->get('totalPrice');
+    }
+
+    public function setTotalPrice($v)
+    {
+        $this->parameters->set('totalPrice',$v);
+    }
+}
