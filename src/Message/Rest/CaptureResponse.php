e7f457d6e938286614e0c7e479e479fb36d6cca7
Sujip Thapa <sudiptpa@gmail.com>
2022-12-26 14:09:08 +0545

    Added Order status after capture.

--- src/Message/Rest/CaptureResponse.php
+++ src/Message/Rest/CaptureResponse.php
@@ -19,8 +19,28 @@ class CaptureResponse extends AbstractResponse
         return isset($this->data['orderId']) ? $this->data['orderId'] : null;
     }
 
+    public function getOrderStatus()
+    {
+        return isset($this->data['orderStatus']) ? (int) $this->data['orderStatus'] : null;
+    }
+
+    public function isApproved()
+    {
+        return in_array($this->getOrderStatus(), ['Approved']);
+    }
+
+    public function getPlanStatus()
+    {
+        return isset($this->data['planStatus']) ? (int) $this->data['planStatus'] : null;
+    }
+
     public function getPurchasePrice()
     {
         return isset($this->data['purchasePrice']) ? (int) $this->data['purchasePrice'] : null;
+    }
+
+    public function getRetailerAmount()
+    {
+        return isset($this->data['retailerAmount']) ? (int) $this->data['retailerAmount'] : null;
     }
 }
