b14e89ab506695c429dc0a912986312b15cc5272
Sujip Thapa <sudiptpa@gmail.com>
2022-12-27 12:46:26 +0545

    Added tests, and fixes in Capture, FetchTransaction responses.

--- src/Message/Rest/FetchTransactionResponse.php
+++ src/Message/Rest/FetchTransactionResponse.php
@@ -16,21 +16,31 @@ class FetchTransactionResponse extends AbstractResponse
 
     public function getOrderId()
     {
-        return isset($this->data['orderId']) ? (string) $this->data['orderId'] : null;
+        return isset($this->data['orderId']) ? $this->data['orderId'] : null;
     }
 
     public function getOrderStatus()
     {
-        return isset($this->data['orderStatus']) ? (string) $this->data['orderStatus'] : null;
+        return isset($this->data['orderStatus']) ? $this->data['orderStatus'] : null;
+    }
+
+    public function isApproved()
+    {
+        return in_array($this->getOrderStatus(), ['Approved']);
     }
 
     public function getPlanStatus()
     {
-        return isset($this->data['planStatus']) ? (string) $this->data['planStatus'] : null;
+        return isset($this->data['planStatus']) ? $this->data['planStatus'] : null;
     }
 
     public function getPurchasePrice()
     {
-        return isset($this->data['purchasePrice']) ? (int) $this->data['purchasePrice'] : null;
+        return isset($this->data['purchasePrice']) ? $this->data['purchasePrice'] : null;
+    }
+
+    public function getRetailerAmount()
+    {
+        return isset($this->data['retailerAmount']) ? $this->data['retailerAmount'] : null;
     }
 }
